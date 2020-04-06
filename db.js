373f1268d16186c32013169749da18f1a902c917
Ryan Hall <ryanhall999@gmail.com>
2020-04-06 19:03:33 -0400

    first commit

--- db.js
+++ db.js
@@ -6,62 +6,8 @@ const client = new pg.Client(
 
 client.connect();
 
-const sync = async () => {
-	const SQL = `
-  DROP TABLE IF EXISTS movies;
-
-  CREATE TABLE movies(
-    id SERIAL,
-    Title VARCHAR(255) NOT NULL UNIQUE,
-    Year VARCHAR(255),
-    Poster VARCHAR(65535),
-    Overview VARCHAR(65535),
-    UserRating FLOAT,
-    VoteAVG FLOAT,
-		Background VARCHAR(65535),
-		DateWatched DATE
-  );
-  INSERT INTO movies (Title, Year, Poster, Overview, UserRating, VoteAvg, Background, DateWatched) VALUES ('Parasite', '2019-11-8',
-   'https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg
-  ', '"All unemployed, Ki-taeks family takes peculiar interest in the wealthy and glamorous Parks for their livelihood until they get entangled in an unexpected incident."', '9', '8.6', '/TU9NIjwzjoKPwQHoHshkFcQUCG.jpg', '2020-03-08');`;
-	client.query(SQL);
-};
-
-const readMovies = async () => {
-	const SQL = "SELECT * from movies";
-	return (await client.query(SQL)).rows;
-};
-
-const addMovie = async movie => {
-	const SQL =
-		"INSERT INTO movies(Title, Year, Poster, Overview, VoteAVG, Background, DateWatched) values($1, $2, $3, $4, $5, $6, $7) returning *";
-	return (
-		await client.query(SQL, [
-			movie.original_title,
-			movie.release_date,
-			movie.poster,
-			movie.overview,
-			movie.vote_average,
-			movie.backdrop_path,
-			movie.dateWatched
-		])
-	).rows[0];
-};
-
-const delMovie = async id => {
-	const SQL = "DELETE FROM movies where id= $1";
-	await client.query(SQL, [id]);
-};
-
-const upMovie = async (id, rating) => {
-	const SQL = "UPDATE movies SET UserRating = $1 WHERE id = $2 returning *;";
-	return (await client.query(SQL, [rating, id])).rows[0];
-};
+const sync = async () => {};
 
 module.exports = {
-	sync,
-	readMovies,
-	addMovie,
-	delMovie,
-	upMovie
+	sync
 };
--- server.js
+++ server.js
@@ -15,30 +15,6 @@ app.get("/", (req, res, next) => {
 	res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.get("/api/movies", (req, res, next) => {
-	db.readMovies()
-		.then(movies => res.send(movies))
-		.catch(next);
-});
-
-app.post("/api/movies", (req, res, next) => {
-	db.addMovie(req.body)
-		.then(movies => res.send(movies))
-		.catch(next);
-});
-
-app.delete("/api/movies/:id", (req, res, next) => {
-	db.delMovie(req.params.id)
-		.then(movies => res.send(movies))
-		.catch(next);
-});
-
-app.put("/api/movies/", (req, res, next) => {
-	db.upMovie(req.body.id, req.body.rating)
-		.then(movies => res.send(movies))
-		.catch(next);
-});
-
 const port = process.env.PORT || 3000;
 
 db.sync().then(() => {
